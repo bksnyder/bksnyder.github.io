b2156051fd82f3fbd09d65a46a3568d7948936cc
bksnyder <bethany.snyder@gmail.com>
2017-11-14 10:15:20 -0500

    More JS

--- final-project/js/app.js
+++ final-project/js/app.js
@@ -3,29 +3,49 @@ $('#media-2').hide();
 $('#media-3').hide();
 $('#click-again').hide();
 
-
-var myMedia = ['nic', 'pizza', 'portugal'];
+var nic = $('#media-1');
+var pizza = $('#media-2');
+var myMedia = [nic, pizza];
+var counter = 0;
 
 
 
 $('#click-me').click(firstClick);
-$('#click-again').click(nextClick);
 
-function firstClick() {
-	$('#media-1').show();
-	$('.description').hide();
-	$('#click-me').hide();
-	$('#click-again').show();
+console.log (counter)
+function firstClick (){
+	if (counter === 0) {
+		myMedia[counter].show();
+		myMedia[myMedia.length - 1].hide();
+	}
+	else {
+		myMedia[counter].hide();
+		myMedia[counter + 1].show();	
+	}
 
+	counter = counter + 1;
+	console.log (counter)
+	if (counter === myMedia.length) {
+		counter = 0;
+	}
 }
 
-function nextClick() {
-	$('#media-1').hide();
-	$('#media-2').show();
-}
+// $('#click-again').click(nextClick);
+
+// function firstClick() {
+// 	$('#media-1').show();
+// 	$('.description').hide();
+// 	$('#click-me').hide();
+// 	$('#click-again').show();
+// }
+
+// function nextClick() {
+// 	$('#media-1').hide();
+// 	$('#media-2').show();
+// }
 
 
-
+//set up a counter
 
 // Upon page load:
 // display opening .description and #click-me button
